93ac5344d7cacad977fc5df65fc161e380101990
ngoclan1007 <n2nlan@gmail.com>
2022-02-13 14:10:09 +0700

    BT ve nha 1- bmi

---
+++ src/Bmi.java
@@ -0,0 +1,8 @@
+public class Bmi {
+    // tính Bmi trả về theo phương thức double có truyền tham số
+    public static double Bmi3(double height, double weight) {
+        double resultBmi3 = weight / (height * height);
+        return resultBmi3;
+    }
+
+}
---
+++ src/Main.java
@@ -0,0 +1,21 @@
+public abstract class Main {
+    // tính Bmi trả về theo phương thức double có truyền tham số
+
+    public static void main(String[] args) {
+        double result3 = Bmi.Bmi3(1.58, 58);
+        System.out.println(" BMI 3 của tôi la " + result3);
+
+    }
+
+
+// theo phương thức Void???????
+//    public static void main(String[] args){
+//        Bmi1(2.0, 50);
+//    }
+//    public static void Bmi1(double height1, double weight1){
+//        double resultBmi1 = weight1/(height1*height1);
+//        System.out.println(" BMi1 của tôi là " + resultBmi1);
+//    }
+
+
+}
